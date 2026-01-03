8dd42593317ef79735a0730eb38500150ffca2f9
Opeyemi Ojo <54722162+opyjo@users.noreply.github.com>
2026-01-03 12:06:29 -0500

    added middleware to the project

---
+++ middleware.ts
@@ -0,0 +1,69 @@
+import { createServerClient } from "@supabase/ssr";
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+	let supabaseResponse = NextResponse.next({
+		request,
+	});
+
+	const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+	const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+	if (!supabaseUrl || !supabaseAnonKey) {
+		return supabaseResponse;
+	}
+
+	const supabase = createServerClient(supabaseUrl, supabaseAnonKey, {
+		cookies: {
+			getAll() {
+				return request.cookies.getAll();
+			},
+			setAll(cookiesToSet) {
+				for (const { name, value } of cookiesToSet) {
+					request.cookies.set(name, value);
+				}
+				supabaseResponse = NextResponse.next({
+					request,
+				});
+				for (const { name, value, options } of cookiesToSet) {
+					supabaseResponse.cookies.set(name, value, options);
+				}
+			},
+		},
+	});
+
+	// IMPORTANT: Do not add any code between createServerClient and
+	// supabase.auth.getUser(). A simple mistake could make it very hard to debug
+	// issues with users being randomly logged out.
+
+	// Refresh the auth token by calling getUser
+	await supabase.auth.getUser();
+
+	// IMPORTANT: You *must* return the supabaseResponse object as it is.
+	// If you're creating a new response object with NextResponse.next() make sure to:
+	// 1. Pass the request in it, like so:
+	//    const myNewResponse = NextResponse.next({ request })
+	// 2. Copy over the cookies, like so:
+	//    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
+	// 3. Change the myNewResponse object to fit your needs, but avoid changing
+	//    the cookies!
+	// 4. Finally:
+	//    return myNewResponse
+	// If this is not done, you may be causing the browser and server to go out
+	// of sync and terminate the user's session prematurely!
+
+	return supabaseResponse;
+}
+
+export const config = {
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 * - public folder assets
+		 */
+		"/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|mp4|webm)$).*)",
+	],
+};
